42e1d13aa177bc732ed19b96fb8b7075a72c3330
Sawyron <s4pakvv@gmail.com>
2024-07-17 17:53:42 +0700

    make field value optional in meal response

--- the-mealdb-api/src/main/java/org/sawyron/themealdbapi/models/MealResponse.kt
+++ the-mealdb-api/src/main/java/org/sawyron/themealdbapi/models/MealResponse.kt
@@ -4,4 +4,4 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MealResponse(@SerialName("meals") val meals: List<Map<String, String>>)
+data class MealResponse(@SerialName("meals") val meals: List<Map<String, String?>>)
